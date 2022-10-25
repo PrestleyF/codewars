cc766d7f757849027f0b84323e8ca10f8ff6ba7e
Prestley <priestlypriest@gmail.com>
2022-10-25 15:55:00 +0530

    added

---
+++ 7 kyu/returning-an-objects-property/code-solution.js
@@ -0,0 +1,23 @@
+// mplement a function which takes a sequence of objects and a property name, and returns a sequence containing the named property of each object.
+
+// For example:
+
+// pluck([{a:1}, {a:2}], 'a')      // -> [1,2]
+// pluck([{a:1, b:3}, {a:2}], 'b') // -> [3, undefined]
+
+// my solution
+
+function pluck(objs, name) {
+    let array = [];
+     for (let i = 0; i < objs.length; i++){
+       array.push(objs[i][name]);
+     }
+     return array
+   
+   }
+
+// alternative solution 
+function pluck(objs, name) {
+    return objs.map(function(obj) { return obj[name] });
+  }
+  
