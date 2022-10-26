619c2b577c39dbeea84fce60f98865e727093d16
Prestley <priestlypriest@gmail.com>
2022-10-26 20:37:34 +0530

    added

---
+++ 7 kyu/increment-each-subsequent-letter-by-1/code-solutions.js
@@ -0,0 +1,18 @@
+// Code Example: 
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt" 
+
+//my solution 
+function accum(s) {
+    let letters = s.split(''), words = [];
+    for (let i = 0; i < letters.length; i++){
+      words.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()))
+    }
+    return words.join('-')
+  }
+
+  // alternative
+  function accum(s) {
+    return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+  }
