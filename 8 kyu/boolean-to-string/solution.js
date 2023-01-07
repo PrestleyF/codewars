df68ac02f19c1ad9eab9978bb20a325766cc1239
Prestley <priestlypriest@gmail.com>
2023-01-07 22:09:02 +0530

    added

---
+++ 8 kyu/boolean-to-string/solution.js
@@ -0,0 +1,12 @@
+
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    if (b) return "true"
+    return "false"
+  } 
+//or
+
+function booleanToString(b){
+    return b.toString();
+}
