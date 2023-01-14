d6eb287574932e3de86c21f158b5e7b78017661b
Prestley <priestlypriest@gmail.com>
2023-01-14 22:20:57 +0530

    added

---
+++ 7 kyu/parameter-sequence/solution.js
@@ -0,0 +1,15 @@
+// Perimeter sequence
+// The first three stages of a sequence are shown.
+
+// blocks
+
+// The blocksize is a by a and a ≥ 1.
+
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+// MATHEMATICSPUZZLES
+
+// my solution 
+function perimeterSequence(a,n) {
+    return a * n * 4
+  }
