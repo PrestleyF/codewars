40692a096b4b10508d601ab89335646ba60eab5d
Prestley <priestlypriest@gmail.com>
2023-03-15 23:00:34 +0530

    added

--- 7 kyu/increment-each-subsequent-letter-by-1/code-solutions.js
+++ 7 kyu/increment-each-subsequent-letter-by-1/code-solutions.js
@@ -12,7 +12,7 @@ function accum(s) {
     return words.join('-')
   }
 
-  // alternative
+  // alternative solution 
   function accum(s) {
     return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
   }
