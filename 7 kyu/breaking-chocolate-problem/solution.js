f66d3a4f2c844993783b0e1bb1ac4891a50aa236
Prestley <priestlypriest@gmail.com>
2023-04-11 23:49:45 +0530

    added

--- 7 kyu/breaking-chocolate-problem/solution.js
+++ 7 kyu/breaking-chocolate-problem/solution.js
@@ -5,5 +5,5 @@
 // If input data is invalid you should return 0 (as in no breaks are needed if we do not have any chocolate to split). Input will always be a non-negative integer.
 
 
-// solution 
+// my solution 
 const breakChocolate = (n,m) => n * m == 0 ? 0 : n * m - 1
