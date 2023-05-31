7fcd159e774b96aabf78782b44e1a7abc0ea1263
Prestley <priestlypriest@gmail.com>
2023-05-31 17:15:34 +0530

    Co-authored-by: Andersen <rickansen@users.noreply.github.com>

---
+++ 8 kyu/Make-Upper-Case/solution.js
@@ -0,0 +1,11 @@
+// Write a function which converts the input string to uppercase.
+
+const makeUpperCase = (str) => str.toUpperCase()
+
+
+
+
+console.log(makeUpperCase(""),           "");
+console.log(makeUpperCase("hello"), "HELLO");
+console.log(makeUpperCase("Hello"), "HELLO");
+console.log(makeUpperCase("HELLO"), "HELLO");
