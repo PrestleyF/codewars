15fa0c9785921dc39c4a946d726f1231ccc5f372
Prestley <priestlypriest@gmail.com>
2023-07-08 19:31:45 +0530

    added

---
+++ 7 kyu/Maximum-Multiple/solution.js
@@ -0,0 +1,26 @@
+// Task
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+// Conditions :
+// N is divisible by divisor
+
+// N is less than or equal to bound
+
+// N is greater than 0.
+
+
+ 
+function maxMultiple(divisor, bound){
+  let N = bound;
+
+  if(N % divisor !== 0 && N <= bound && N > 0 ){
+    return N--;
+  } 
+  return N;
+}
+
+// or
+
+function maxMultiple(divisor, bound){
+  bound-bound % divisor
+}
