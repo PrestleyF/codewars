54d2d0bf28178a3f98b33a7b53bc379c278a8ce3
Prestley <priestlypriest@gmail.com>
2023-07-26 20:13:09 +0530

    made corrections to answer

--- 6 kyu/Who likes it/solution.js
+++ 6 kyu/Who likes it/solution.js
@@ -21,7 +21,7 @@ function likes(names) {
       return `${names[0]}, ${names[1]} and ${names[2]} like this`
     }
      else {
-       return `${names[0]}, ${names[1]}, and ${names.length - 2} others like this`;
+       return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
      }
      }
 
