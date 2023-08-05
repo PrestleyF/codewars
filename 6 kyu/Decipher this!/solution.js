600e0e281ec534a3cdf5c6743c6342527140c763
Prestley <priestlypriest@gmail.com>
2023-08-05 21:40:25 +0530

    added

--- 6 kyu/Decipher this!/solution.js
+++ 6 kyu/Decipher this!/solution.js
@@ -21,7 +21,9 @@
 function decipherThis(str) {
   const word = str.replace(/^(.)(.*)(.)$/, '$3$2$1');
   return word;
-    
+ 
+  
+  
 }; 
 
 console.log(decipherThis('72eva 97 103o 97t 116sih 97dn 115ee 104wo 121uo 100o'), 'Have a go at this and see how you do'); 
