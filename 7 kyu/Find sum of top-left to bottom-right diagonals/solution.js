1c818d29a0741c77c44fde5469ccc7d9a2ab9cf1
Prestley <priestlypriest@gmail.com>
2024-01-25 18:37:32 +0100

    added

--- 7 kyu/Find sum of top-left to bottom-right diagonals/solution.js
+++ 7 kyu/Find sum of top-left to bottom-right diagonals/solution.js
@@ -14,9 +14,17 @@
 
 function diagonalSum(matrix){
  
-let numberOfArrays = matrix.length    
+let numberOfArrays = matrix.length 
 
+let sum = 0;
+
+for(let i = 0; i < numberOfArrays; i++){
+sum += matrix[i][i]
 }
+return sum
+}
+
+
 
   console.log(diagonalSum([[12]]), 12)
   console.log(diagonalSum([[1, 2], [3, 4]]), 5)
